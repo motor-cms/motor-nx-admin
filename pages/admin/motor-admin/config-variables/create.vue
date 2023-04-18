cb779f783838054e6ecb8c4c83015bc5684dd163
Lukas Roth <mail@lukasroth.net>
2023-04-18 13:11:10 +0200

    config variables to syntactic sugar

--- pages/admin/motor-admin/config-variables/create.vue
+++ pages/admin/motor-admin/config-variables/create.vue
@@ -47,36 +47,15 @@
     </div>
   </AdminCommonForm>
 </template>
-<script lang="ts">
-import { defineComponent, ref } from 'vue'
-import AdminCommonForm from '@zrm/motor-nx-core/components/admin/common/Form.vue'
-import FormsInputField from '@zrm/motor-nx-core/components/forms/InputField.vue'
-import FormsTextAreaField from '@zrm/motor-nx-core/components/forms/TextAreaField.vue'
-import { useI18n } from 'vue-i18n'
+<script setup lang="ts">
+import {useI18n} from 'vue-i18n'
 import form from '@zrm/motor-nx-admin/forms/configVariableForm'
+// Load i18n module
+const {t} = useI18n()
 
-export default defineComponent({
-  name: 'admin-motor-admin-config-variables-create',
-  components: {
-    AdminCommonForm,
-    FormsInputField,
-    FormsTextAreaField,
-  },
-  setup() {
-    // Load i18n module
-    const { t } = useI18n()
+// Load form
+const {model, onSubmit} = form()
 
-    // Load form
-    const { model, onSubmit } = form()
-
-    // Set default action title
-    const title = ref(t('motor-admin.config_variables.create'))
-
-    return {
-      model,
-      title,
-      onSubmit,
-    }
-  },
-})
+// Set default action title
+const title = ref(t('motor-admin.config_variables.create'))
 </script>
--- pages/admin/motor-admin/config-variables/index.vue
+++ pages/admin/motor-admin/config-variables/index.vue
@@ -14,81 +14,58 @@
   ></AdminCommonGrid>
 </template>
 
-<script lang="ts">
-import {useAppStore} from "~/packages/motor-nx-core/store/app";
-import AdminCommonGrid from '@zrm/motor-nx-core/components/admin/common/Grid.vue'
-import { defineComponent, ref } from 'vue'
-import { useI18n } from 'vue-i18n'
-import EditButton from '@zrm/motor-nx-core/components/admin/cell/EditButton.vue'
-import DeleteButton from '@zrm/motor-nx-core/components/admin/cell/DeleteButton.vue'
+<script setup lang="ts">
+import {useI18n} from 'vue-i18n'
 import grid from '@zrm/motor-nx-admin/grids/configVariableGrid'
 
-export default defineComponent({
-  name: 'admin-motor-admin.config-variables',
-  components: {
-    AdminCommonGrid,
+// Load i18n module
+const {t} = useI18n()
+
+// Define columns for grid
+const columns = ref([
+  {
+    name: t('motor-admin.config_variables.package'),
+    prop: 'package',
   },
-  setup() {
-    // Load i18n module
-    const { t } = useI18n()
-
-    // Define columns for grid
-    const columns = ref([
-      {
-        name: t('motor-admin.config_variables.package'),
-        prop: 'package',
-      },
-      {
-        name: t('motor-admin.config_variables.group'),
-        prop: 'group',
-      },
-      {
-        name: t('motor-admin.config_variables.name'),
-        prop: 'name',
-      },
-      {
-        name: t('motor-admin.config_variables.value'),
-        prop: 'value',
-      },
-      {
-        name: '',
-        prop: 'actions',
-        columnStyle: 'width: 200px',
-        rowWrapperClass: 'justify-content-end',
-        components: [
-          {
-            name: 'EditButton',
-            options: {
-              route: 'admin.motor-admin.config-variables.edit',
-              name: t('global.edit'),
-            },
-          },
-          { name: 'DeleteButton', options: { name: t('global.delete') } },
-        ],
-      },
-    ])
-
-    // Define filters for grid
-    const filters = ref([{ name: 'SearchFilter', options: {} }])
-
-    const loadComponents = <any>[]
-
-    // WE START THE OUTSOURCED CODE HERE
-    const { rows, meta, refreshRecords, handleCellEvent } = grid()
-
-    onMounted(async () => {
-      await refreshRecords();
-})
-
-    return {
-      columns,
-      filters,
-      rows,
-      meta,
-      refreshRecords,
-      loadComponents,
-      handleCellEvent,
-    }
+  {
+    name: t('motor-admin.config_variables.group'),
+    prop: 'group',
   },
+  {
+    name: t('motor-admin.config_variables.name'),
+    prop: 'name',
+  },
+  {
+    name: t('motor-admin.config_variables.value'),
+    prop: 'value',
+  },
+  {
+    name: '',
+    prop: 'actions',
+    columnStyle: 'width: 200px',
+    rowWrapperClass: 'justify-content-end',
+    components: [
+      {
+        name: 'EditButton',
+        options: {
+          route: 'admin.motor-admin.config-variables.edit',
+          name: t('global.edit'),
+        },
+      },
+      {name: 'DeleteButton', options: {name: t('global.delete')}},
+    ],
+  },
+])
+
+// Define filters for grid
+const filters = ref([{name: 'SearchFilter', options: {}}])
+
+const loadComponents = <any>[]
+
+// WE START THE OUTSOURCED CODE HERE
+const {rows, meta, refreshRecords, handleCellEvent} = grid()
+
+onMounted(async () => {
+  await refreshRecords();
 })
 </script>
