db738b2d86e1be99317f874f2884f39ad9c45b2b
Jan Ehrlich <je@zeitraum.com>
2024-01-10 17:30:41 +0100

    Added Ref for Action <-> Product mapping

--- types/formInterfaces.ts
+++ types/formInterfaces.ts
@@ -3,6 +3,12 @@ interface GenericOptionPair {
     value: string;
 }
 
+interface GenericOptionPairWithReference {
+    label: string;
+    value: string;
+    ref: string;
+}
+
 interface GendersPerLanguage {
     [key: string]: GenericOptionPair[];
 }
