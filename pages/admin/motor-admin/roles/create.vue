36c17763c6b2f70fe6212b7420b1834d759cb8c4
Jan Ehrlich <je@zeitraum.com>
2024-06-12 17:32:45 +0200

    Removed Debug Code

--- pages/admin/motor-admin/roles/create.vue
+++ pages/admin/motor-admin/roles/create.vue
@@ -1,11 +1,7 @@
 <template>
-  <AdminCommonForm
-    back-route="admin.motor-admin.roles"
-    :title="title"
-    @submit="onSubmit"
-  >
+  <AdminCommonForm back-route="admin.motor-admin.roles" :title="title" @submit="onSubmit">
     <h6 class="text-uppercase text-body text-xs font-weight-bolder">
-      {{ $t('motor-admin.global.basic_information')}}
+      {{ $t("motor-admin.global.basic_information") }}
     </h6>
     <div class="row">
       <div class="col-md-6">
@@ -35,47 +31,49 @@
           id="permissions"
           :label="$t('motor-admin.permissions.permissions')"
           v-model="model.permissions"
-          :options="permissions.filter(p => p.guard_name == model.guard_name)"
+          :options="permissions.filter((p) => p.guard_name == model.guard_name)"
         ></FormsCheckboxArrayField>
       </div>
     </div>
-    {{permissions}}
+    <!-- {{permissions}} -->
   </AdminCommonForm>
 </template>
 <script setup lang="ts">
-import { useI18n } from 'vue-i18n'
-import form from '@zrm/motor-nx-admin/forms/roleForm'
+import { useI18n } from "vue-i18n";
+import form from "@zrm/motor-nx-admin/forms/roleForm";
 
 // Load i18n module
-const { t } = useI18n()
+const { t } = useI18n();
 
 // Load router
-const router = useRouter()
+const router = useRouter();
 
 // Load form
-const { model, getData, onSubmit, permissions, loadPermissions } = form()
+const { model, getData, onSubmit, permissions, loadPermissions } = form();
 
 // Set default action title
-const title = ref(t('motor-admin.roles.create'))
+const title = ref(t("motor-admin.roles.create"));
 
 // Sanitize roles
 watch(model, () => {
-  const checkAgainst = Object.entries(model.value.permissions)
+  const checkAgainst = Object.entries(model.value.permissions);
 
-  const options = []
+  const options = [];
   for (const object of checkAgainst) {
-    const checkObject: any = object
+    const checkObject: any = object;
     if (checkObject[1]) {
-      options.push(checkObject[1]['id'])
+      options.push(checkObject[1]["id"]);
     }
   }
 
-  model.value.permissions = options
-})
-watch(() => model.value.guard_name, () => {
-  model.value.permissions = [];
+  model.value.permissions = options;
 });
+watch(
+  () => model.value.guard_name,
+  () => {
+    model.value.permissions = [];
+  }
+);
 
 await loadPermissions();
-
 </script>
